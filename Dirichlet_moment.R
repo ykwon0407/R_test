1a0bf1801708744d5cad8412d2b784fbec3b0452
Yongchan Kwon <ykwon.0407@gmail.com>
2016-07-05 19:41:40 +0900

    d

--- Dirichlet_moment.R
+++ Dirichlet_moment.R
@@ -8,14 +8,14 @@ res2 = sum((W-1/n)^2)
 print(c(res1, res2))
 }
 
-M=10^4
+M=10^6
 Y=Z=Q1=Q2=rep(0,M)
-for(n in 1:M){
+for(n in 10^5:M){
   X=rgamma(n, shape = 1)
   W=X/sum(X)
   Y[n] = W[1]-1/n
   Z[n] = sum((W-1/n)^2)
-  Q1[n] = mean((n*W))
+  Q1[n] = mean(n*W)
   Q2[n] = mean((n*W)^2)
 }
 
